ffd1145bbcb911772924bbe55f75fffcc8dc7828
Jay Barredo <38466188+CatapangJay@users.noreply.github.com>
2024-01-26 21:04:31 +0800

    Update quiz.service.ts

--- src/app/service/quiz.service.ts
+++ src/app/service/quiz.service.ts
@@ -1,15 +1,20 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { QuestionInfo } from '../models/question';
+import { TestInfo } from '../models/test';
 
 @Injectable({
   providedIn: 'root'
 })
-export class QuizService {  
+export class QuizService {
 
   constructor(private http: HttpClient) { }
 
-  public getQuestions(){
-    return this.http.get<QuestionInfo[]>('/assets/ace-3.json')
+  public getTests() {
+    return this.http.get<TestInfo[]>('/assets/test-list.json')
+  }
+
+  public getQuestions(testId:number) {
+    return this.http.get<QuestionInfo[]>(`/assets/ace-${testId}.json`)
   }
 }
