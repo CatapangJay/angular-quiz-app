399b8134ad47fd1758d457c0923cea24a7b7f5e3
Jay Barredo <38466188+CatapangJay@users.noreply.github.com>
2024-01-26 20:47:18 +0800

    Update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { QuizComponent } from './pages/quiz/quiz.component';
+import { QuizSelectorComponent } from './pages/quiz-selector/quiz-selector.component';
 
 export const routes: Routes = [
-    { path: 'quiz', component: QuizComponent },
+    { path: 'tests', component: QuizSelectorComponent },
+    { path: 'tests/:id', component: QuizComponent },
 ];
